a3c6322a1d88beceeecdcb30cf04d63288608546
(O.O) <Lorain.Li@icloud.com>
2018-09-01 15:38:36 +0800

    add hook for player action

--- Karma.lua
+++ Karma.lua
@@ -5,24 +5,49 @@ function Initialize(a_Plugin)
 	a_Plugin:SetName("Karma")
 	a_Plugin:SetVersion(1)
 	g_Plugin = a_Plugin
+	Merits:Load()
+	cPluginManager:AddHook(cPluginManager.HOOK_KILLED, MyOnKilled)
 	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_JOINED, MyOnPlayerJoined)
 	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_DESTROYED, MyOnPlayerDestroyed)
-	cPluginManager:AddHook(cPluginManager.HOOK_TAKE_DAMAGE, MyOnTakeDamage)
+	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_BROKEN_BLOCK, MyOnPlayerBrokenBlock)
+	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_PLACED_BLOCK, MyOnPlayerPlacedBlock)
 	return true
 end
 
 function OnDisable()
 	LOG("Karma v" .. g_Plugin:GetVersion() .. " is disabling")
+	Merits:Save()
 end
 
 function MyOnPlayerJoined(a_Player)
-	-- body
+	Merits:PlayerJoin(a_Player)
 end
 
 function MyOnPlayerDestroyed(a_Player)
-	-- body
+	Merits:PlayerQuit(a_Player)
 end
 
-function MyOnTakeDamage(a_Entity, a_TDI)
-	-- body
+function MyOnKilled(a_Victim, a_TDI, a_DeathMessage)
+	if(cMonster:FamilyFromType(a_Victim:GetMonsterType()) == mfHostile) then
+		if(a_TDI.Attacker:IsPlayer()) then
+			Merits:PlayerDoMerits(10)
+		end
+	end
+	if(cMonster:FamilyFromType(a_Victim:GetMonsterType()) == mfPassive) then
+		if(a_TDI.Attacker:IsPlayer()) then
+			Merits:PlayerDoMerits(-10)
+		end
+	end
+end
+
+function MyOnPlayerBrokenBlock(a_Player, a_BlockX, a_BlockY, a_BlockZ, a_BlockFace, a_BlockType, a_BlockMeta)
+	if(a_BlockType == E_BLOCK_LOG or a_BlockType == E_BLOCK_NEW_LOG) then
+		Merits:PlayerDoMerits(-1)
+	end
+end
+
+function MyOnPlayerPlacedBlock(a_Player, a_BlockX, a_BlockY, a_BlockZ, a_BlockFace, a_BlockType, a_BlockMeta)
+	if((a_BlockType == E_BLOCK_LOG or a_BlockType == E_BLOCK_NEW_LOG) and (a_BlockMeta <= E_META_SAPLING_DARK_OAK)) then
+		Merits:PlayerDoMerits(1)
+	end
 end
